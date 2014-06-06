dc60cbebe7520e840b4c17715aa6bd8bf3b767a0
Isao Yagi <isao.yagi@gmail.com>
2014-06-06 16:58:45 -0700

    fix missing comma in var declarations

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var test = require('tape'),
-    bbrb = require('./')
+    bbrb = require('./'),
     pathname = __filename;
 
 test('items show line, reason, file', function(t) {
